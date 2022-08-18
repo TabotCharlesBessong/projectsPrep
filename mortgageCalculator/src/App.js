15f576fd2c542a92d1251daa60b3c854357d28bf
TabotCharlesBessong <ebezebeatrice@gmail.com>
2022-08-18 10:33:40 +0100

    complete

--- mortgageCalculator/src/App.js
+++ mortgageCalculator/src/App.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import "./App.css";
+import { FcHome } from "react-icons/fc";
+import Form from "./components/Form";
+import React from "react";
 
-const App = () => {
-  return (
-    <div>
-      <h1>
-        hello world
-      </h1>
-    </div>
-  )
+function App() {
+	return (
+		<div
+			className="App container"
+			style={{ maxWidth: 500, margin: "1rem auto" ,display: "flex", justifyContent: "center",flexDirection:'column',alignItems:'center'}}
+		>
+			<h1 className="display-1 my-5">
+				<FcHome /> Mortgage Calculator{" "}
+			</h1>
+			<Form />
+		</div>
+	);
 }
 
-export default App
+export default App;
