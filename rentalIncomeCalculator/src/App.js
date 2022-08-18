29d8015c36b458c29b21f9fdd2bbdd39920feaad
TabotCharlesBessong <ebezebeatrice@gmail.com>
2022-08-18 20:15:35 +0100

    building a new calculator

---
+++ rentalIncomeCalculator/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+	return (
+		<div>
+			<h1>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit pariatur amet quod, facere soluta atque odit ea optio delectus earum neque illo ipsa voluptas temporibus. Enim cum ipsa reiciendis delectus, error assumenda, expedita laboriosam iusto natus nulla, aliquid quia. Quae, fugit aspernatur tempora vitae impedit corrupti natus, mollitia ea aperiam sit ipsa? Ratione harum voluptates soluta quaerat possimus perspiciatis amet eum at nemo velit molestias maxime sint quae, deserunt doloremque.
+			</h1>
+		</div>
+	)
+}
+
+export default App
