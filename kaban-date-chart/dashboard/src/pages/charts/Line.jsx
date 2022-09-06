e66fbdde95431336e6cb6c0cff92afbb7d01b0f5
TabotCharlesBessong <ebezebeatrice@gmail.com>
2022-09-06 08:49:52 +0100

    added 3 more chart

--- kaban-date-chart/dashboard/src/pages/charts/Line.jsx
+++ kaban-date-chart/dashboard/src/pages/charts/Line.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import { ChartsHeader, LineChart } from '../../components';
+import { ChartHeader, LineChart } from '../../components';
 
 const Line = () => (
   <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
-    <ChartsHeader category="Line" title="Inflation Rate" />
+    <ChartHeader category="Line" title="Inflation Rate" />
     <div className="w-full">
       <LineChart />
     </div>
